996c9a4b5ef693a15cdf9fb296fce11c65c67e1d
Kiko Barr <34662575+kikobarr@users.noreply.github.com>
2025-03-09 11:16:22 -0700

    Added Artist Search

--- dcrm/website/filters.py
+++
@@ -1,9 +0,0 @@
-import django_filters
-from .models import *
-
-
-class ArtistFilter(django_filters.FilterSet):
-    class Meta:
-        model = Artist
-        fields = ['artist_fname', 'artist_lname']
-
